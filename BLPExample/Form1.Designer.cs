577648aa02ea67f4445e15921213d2d13019e010
Alexander I. Zaytsev <hazzik@gmail.com>
2014-04-24 02:49:06 +1200

    Initial version as found [here](http://www.ownedcore.com/forums/world-of-warcraft/world-of-warcraft-bots-programs/327660-sereniablp-lib-opensource-net-blp-decompression-lib.html)

---
+++ BLPExample/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace BLPExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 427);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load BLP";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(460, 400);
+            this.panel1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(190, 427);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Save as PNG";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(382, 427);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Save as JPG";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(286, 427);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Save as BMP";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 462);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Serenia BLP Lib example";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ BLPExample/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace BLPExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.g = panel1.CreateGraphics();
+        }
+
+        SereniaBLPLib.BlpFile exampleBLP;
+        Bitmap bmp;
+        Graphics g;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.exampleBLP != null)
+                {
+                    this.exampleBLP.close();
+                    this.exampleBLP = null;
+                }
+
+                FileStream file = new FileStream("example.blp", FileMode.Open);
+                this.exampleBLP = new SereniaBLPLib.BlpFile(file);
+                MessageBox.Show("Mipmap count: "+exampleBLP.MipMapCount);
+
+                // loading bitmap level 0
+                bmp = this.exampleBLP.getBitmap(0);
+
+                g.DrawImage(bmp, 0, 0);
+
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+                
+            }
+            catch (FileNotFoundException fe)
+            {
+                MessageBox.Show("The 'example.blp' was not found!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.bmp.Save("example.png", System.Drawing.Imaging.ImageFormat.Png);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.bmp.Save("example.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.bmp.Save("example.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
+    }
+}
